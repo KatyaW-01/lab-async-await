3a4387258d8be1113772c4fb4fd3c95d2c570cd8
Katya Weicht <katya.weicht@gmail.com>
2025-05-21 13:54:25 -0600

    Display posts and call in async function for fetch

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-// Write your code here!
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+async function fetchAndDisplayPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await response.json()
+
+    displayPosts(posts)
+  } catch (error) {
+    console.error('Error fetching posts data:', error)
+  }
+};
+
 function displayPosts(posts) {
   posts.forEach((post) => {
+    const postList = document.querySelector("#post-list")
+
     const list = document.createElement('li')
     const header = document.createElement('h1')
+    header.textContent = post.title
+    const paragraph = document.createElement('p')
+    paragraph.textContent = post.body
+
+    list.append(header)
+    list.append(paragraph)
+    postList.append(list)
   })
-  
 }
+
+fetchAndDisplayPosts()
+
+
