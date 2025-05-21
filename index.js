318f49e90f045604bce8217f9dde650a16927b89
Katya Weicht <katya.weicht@gmail.com>
2025-05-20 19:38:36 -0600

    Start display posts function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+function displayPosts(posts) {
+  posts.forEach((post) => {
+    const list = document.createElement('li')
+    const header = document.createElement('h1')
+  })
+  
+}
